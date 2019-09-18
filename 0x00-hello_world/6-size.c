25396c0f0491e100122665b5e4bc9a82afcb7113
FeliPrado31 <srgatosicool@gmail.com>
2019-09-18 18:45:36 +0000

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,16 @@
+/**
+ * main - print a string using puts
+ *
+ * Return: Always 0 (Success)
+ */
 #include <stdio.h>
 
-int main(void){
-  printf("Size of a char: %ld byte(s)", sizeof(char));
-  printf("Size of an int: %ld byte(s)", sizeof(int));
-  print("Size of a long int: %ld byte(s)", sizeof(long));
-  printf("Size of a long long int: %ld byte(s)", sizeof(long long));
-  printf("Size of a float: %ld byte(s)", sizeof(float));
-  return(0);
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)", sizeof(char));
+	printf("Size of an int: %ld byte(s)", sizeof(int));
+	print("Size of a long int: %ld byte(s)", sizeof(long));
+	printf("Size of a long long int: %ld byte(s)", sizeof(long long));
+	printf("Size of a float: %ld byte(s)", sizeof(float));
+	return (0);
 }
