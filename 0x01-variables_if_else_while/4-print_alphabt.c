2cfbbd869c74d3dfa7ad58e33f52e44671b5beac
FeliPrado31 <srgatosicool@gmail.com>
2019-09-19 19:29:57 +0000

    fix

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: No argument
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != 'q' || c != 'e')
+			putchar(c);
+	putchar('\n');
+	return (0);
+}
