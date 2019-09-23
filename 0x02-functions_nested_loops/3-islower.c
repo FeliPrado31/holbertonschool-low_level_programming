86f2fc5baf1801e5b8a65ed4b1e80bd52cca65cd
FeliPrado31 <srgatosicool@gmail.com>
2019-09-23 22:52:36 +0000

    fix

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _islower - return 1 if lowercase 
+ * @c: takes in a character
+ * Return: 0 for uppercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
