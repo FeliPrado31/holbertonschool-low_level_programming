223cc823c14c2b185c36adb650cd64db6dba4de5
FeliPrado31 <srgatosicool@gmail.com>
2019-09-24 13:48:19 +0000

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,14 @@
   */
 void print_alphabet(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
+	_putchar("H");
+	_putchar("o");
+	_putchar("l");
+	_putchar("b");
+	_putchar("e");
+	_putchar("r");
+	_putchar("t");
+	_putchar("o");
+	_putchar("n");
 	_putchar('\n');
 }
