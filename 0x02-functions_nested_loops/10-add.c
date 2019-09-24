a5fc6fadccab855361d7ba04647a4e6ff23f653a
FeliPrado31 <srgatosicool@gmail.com>
2019-09-24 13:43:15 +0000

    fix

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * sum - sum two numbers
+ * @a: number one
+ * @b: number two
+ * Return: sum of int one/int two
+ */
+int sum(int a, int b)
+{
+	a += b;
+	return (a);
+}
