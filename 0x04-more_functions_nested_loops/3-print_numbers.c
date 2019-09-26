8ae74dbca11ce304eaf700605d91c243119ef246
FeliPrado31 <srgatosicool@gmail.com>
2019-09-26 14:53:10 +0000

    fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * @void: no argument
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
