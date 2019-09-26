9bc137eeacf4f88a6e4afdef44cdf09dd883e116
FeliPrado31 <srgatosicool@gmail.com>
2019-09-26 17:59:20 +0000

    fix

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_line - Write a function that draws a straight line in the terminal.
+ * @n: take an int
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('.');
+			for (j = 2; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+		}
+	_putchar(92);
+	_putchar('\n');
+	}
+}
