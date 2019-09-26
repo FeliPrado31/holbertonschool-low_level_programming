9f70dd4817472d70ba0aa376e3d372a9f798ac36
FeliPrado31 <srgatosicool@gmail.com>
2019-09-26 15:29:17 +0000

    fix

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_line - Write a function that draws a straight line in the terminal.
+ * @n: take an int
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
+	}
+}
