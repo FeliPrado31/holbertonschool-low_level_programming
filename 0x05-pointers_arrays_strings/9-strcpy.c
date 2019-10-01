b90c3a9330b77a1e273c0b2e674ddd704b49c846
FeliPrado31 <srgatosicool@gmail.com>
2019-10-01 20:25:01 +0000

    fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,19 +1,6 @@
 #include "holberton.h"
 
 /**
- * array_length - check the long of the string
- * @s: take an pointer
- * Return: size of the string
- */
-int array_length(char *s)
-{
-	int size;
-
-	for (size = 0; s[size]; size++)
-		;
-	return (size);
-}
-/**
  * _strcpy - Write a function that copies the string pointed to by src
  * @dest: pointer
  * @src: pointer
@@ -23,7 +10,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i, size;
 
-	size = array_length(src);
+	for (size = 0; src[size]; size++)
+		;
+
 	for (i = 0; i < size; i++)
 		dest[i] = src[i];
 	return (dest);
