5a49388a78cd5e34f9e0d41eb5bc70695ebaa3d3
FeliPrado31 <srgatosicool@gmail.com>
2019-10-01 19:12:38 +0000

    fix

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * array_length - check the long of the string
+ * @s: take an pointer
+ * Return: size of the string
+ */
+int array_length(char *s)
+{
+	int size;
+
+	for (size = 0; s[size]; size++)
+		;
+	return (size);
+}
+
+/**
+ * puts_half - Write a function that prints half of a string
+ * @str: take an pointer
+ */
+void puts_half(char *str)
+{
+	int i, size;
+
+	size = array_length(str);
+
+	for (i = size / 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
