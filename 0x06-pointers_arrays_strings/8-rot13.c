2eb8b15d69dfd260c206bbea1d0ae217af942d17
FeliPrado31 <srgatosicool@gmail.com>
2019-10-03 20:38:28 +0000

    fix

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+  * rot13 - encodes string to rot13
+  * @c: takes in a string
+  * Return: the string
+  */
+char *rot13(char *c)
+{
+	int i, j;
+
+	char *c1 = "abcdefghijklmABCDEFGHIJKLM";
+	char *c2 = "nopqrstuvwxyzNOPQRSTUVWXYZ";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; c1[j] != '\0'; j++)
+		{
+			if (c[i] == c1[j])
+			{
+				c[i] = c2[j];
+			}
+			else if (c[i] == c2[j])
+			{
+				c[i] = c1[j];
+			}
+		}
+	}
+	return (c);
+}
