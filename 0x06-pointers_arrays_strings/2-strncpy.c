78b0722c62d5577f9219608112e1803b4fcd27fe
FeliPrado31 <srgatosicool@gmail.com>
2019-10-03 17:58:34 +0000

    fix

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,42 @@
+/*
+ * The  strcpy()  function  copies the string pointed to by src,
+ including the terminating null byte ('\0'),
+ to the buffer  pointed  to  by  dest.
+ The  strings  may  not overlap,
+ and the destination string dest must be large enough to receive the copy.
+ Beware  of  buffer  overruns!
+ */
+
+#include "holberton.h"
+
+/**
+ * array_length - check the long of the string
+ * @s: take an pointer
+ * Return: size of the string
+ */
+int array_length(char *s)
+{
+	int size;
+
+	for (size = 0; s[size]; size++)
+		;
+	return (size);
+}
+/**
+ * *_strncpy - Write a function that copies a string
+ * @dest: take an pointer
+ * @src: take an pointer
+ * @n: size of the function
+ * Return: the string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, size;
+
+	size = array_length(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; n > size; size++)
+		dest[size] = '\0';
+	return (dest);
+}
