9d00337e7e44a5457942c627f4b9a508e5072870
FeliPrado31 <srgatosicool@gmail.com>
2019-10-07 15:38:51 +0000

    fix

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _memset - Write a function that fills memory with a constant byte.
+ * @s: pointer
+ * @b: char
+ * @n: lenght
+ * Return: 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n-- > 0)
+		*ptr++ = b;
+	return s;
+}
