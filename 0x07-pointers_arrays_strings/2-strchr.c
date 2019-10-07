52814a62031d589d84e6cac6b1da68ab2fd123bb
FeliPrado31 <srgatosicool@gmail.com>
2019-10-07 18:07:40 +0000

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 
 /**
- *
+ * _strchr - Write a function that locates a character in a string.
+ * @s: pointer
+ * @c: char
+ * Return: 0
  */
 char *_strchr(char *s, char c)
 {
 	do {
 		if (*s == c)
 		{
-			return (char*)s;
+			return (char *s);
 		}
 	} while (*s++);
 	return (0);
