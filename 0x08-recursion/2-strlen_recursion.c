61b436dfd15ba5d8166de6a4bd5223e59f4ee612
FeliPrado31 <srgatosicool@gmail.com>
2019-10-09 14:57:37 +0000

    fix

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: ponter
+ * Return: sum 1;
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
