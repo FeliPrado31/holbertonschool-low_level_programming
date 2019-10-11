8d16b78fada0ef4dafc5f85e8185892def41a527
FeliPrado31 <srgatosicool@gmail.com>
2019-10-11 15:08:03 +0000

    fix

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: idk
+ * @argv: idk
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
