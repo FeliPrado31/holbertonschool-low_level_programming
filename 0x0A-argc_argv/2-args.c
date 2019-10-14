ebfd4378c277b03f68c57e1b42cf87d8b96ceda0
FeliPrado31 <srgatosicool@gmail.com>
2019-10-14 03:52:54 +0000

    fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,16 +1,16 @@
-#include "holberton.h"
 #include <stdio.h>
 /**
-  * main - program that prints all arguments it receives.
-  * @argc: takes in a string
-  * @argv: takes in array
-  * Return: 0
-  */
+ * main - Program that prints the number of arguments passed into it.
+ * @argc: Number of arguments.
+ * @argv: Array with arguments in memory.
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
+
 	return (0);
 }
