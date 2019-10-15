c9b532ee9fa0f755e7c07fb3b24d03a8dbc34731
FeliPrado31 <srgatosicool@gmail.com>
2019-10-15 21:04:17 +0000

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,46 +1,44 @@
-#include <stdlib.h>
-
-
+#include<stdio.h>
+#include<stdlib.h>
+#include"holberton.h"
 /**
- * _strlen - Write a function that returns the length of a string.
- * @s: take an pointer
- * Return: the index of the array
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i]; i++)
-		;
-	return (i);
-}
-/**
- * str_concat - function that concatenates two strings.
- * @s1: string 1
- * @s2: string 2
- * Return: string
- */
+* str_concat - concatenates two strings
+* @s1: char s1
+* @s2: char s2
+*
+* Return: arr
+*/
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int s1Len = 0, s2Len = 0, totalLen;
-	char *str, *strFinal;
+	char *arr;
+	int i, j, k, l;
 
 	if (s1 == NULL)
-		s1 = "";
-	s1Len = _strlen(s1);
+	{
+	s1 = "";
+	}
 	if (s2 == NULL)
-		s2 = "";
-	s2Len = _strlen(s2);
-	totalLen = s1Len + s2Len + 1;
-	str = malloc(totalLen + sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	strFinal = str;
-	while (*s1)
-		*str++ = *s1++;
-	while (*s2)
-		*str++ = *s2++;
-	*str = '\0';
-	return (strFinal);
-
+	{
+	s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+	arr = malloc(sizeof(*arr) * (i + j + 1));
+	if (arr == NULL)
+	{
+		return ('\0');
+	}
+	for (k = 0; k < i; k++)
+	{
+		arr[k] = s1[k];
+	}
+	for (l = 0; l < j; l++)
+	{
+		arr[i + l] = s2[l];
+	}
+	return (arr);
 }
