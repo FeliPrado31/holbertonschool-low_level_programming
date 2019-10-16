56693f509f8e6768c2c7d4fe8cfa105bf472ebb3
FeliPrado31 <srgatosicool@gmail.com>
2019-10-16 03:17:09 +0000

    fix

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+/**
+ * _strlen - Write a function that returns the length of a string.
+ * @s: take an pointer
+ * Return: the index of the array
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ *
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, args = 0;
+	char *s;
+
+	if (ac <= 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		args += _strlen(av[i]) + 1;
+	s = malloc(sizeof(char) * args + 1);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0, k = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+			s[k] = av[i][j];
+		s[k] = '\n';
+		k++;
+	}
+	s[k] = '\0';
+	return (s);
+}
