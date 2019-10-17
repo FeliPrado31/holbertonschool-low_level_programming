a08cd9f7ede1e0e92980efb5b77b5f40382e1e74
FeliPrado31 <srgatosicool@gmail.com>
2019-10-17 21:36:31 +0000

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (a == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size * nmemb; i++)
 		a[i] = 0;
 	return (a);
 }
