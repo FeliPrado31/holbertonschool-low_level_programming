b11f4cf81a9e68f0b387b83127433f47a0cf7980
FeliPrado31 <srgatosicool@gmail.com>
2019-10-21 17:55:28 +0000

    fix

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOGFILE
+#define DOGFILE
+/**
+ * struct dog - structure of a dog.
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
