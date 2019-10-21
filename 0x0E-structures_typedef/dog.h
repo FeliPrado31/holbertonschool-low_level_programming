2cc96d35185bfe05dda5a621e03008260224d6f0
FeliPrado31 <srgatosicool@gmail.com>
2019-10-21 18:01:43 +0000

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,21 @@
-#ifndef DOGFILE
-#define DOGFILE
+#ifndef DOGHEADER
+#define DOGHEADER
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * struct dog - structure of a dog.
- * @name: name
- * @age: age
- * @owner: owner
+ * struct dog - this is a structure of a dog.
+ * @name: the name of the dog
+ * @age: the name of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: this structure defines some characteristics that a dog has.
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+
+};
+
 #endif
