afcd2f79025fe55bb2a5e858418445a30301f975
FeliPrado31 <srgatosicool@gmail.com>
2019-10-23 15:32:18 +0000

    fix

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ *
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	/* compare the operator and return the function  */
+	for (i = 0; ops.[i].op != NULL; i++)
+	{
+		if (strcmp(ops.[i].op, s) == 0)
+			return (ops[i].f);
+	}
+	printf("Error\n");
+	exit(98);
+}
