f27a7f9e50ca996cab63755cdcfe583ea7310c7f
FeliPrado31 <srgatosicool@gmail.com>
2019-11-05 15:03:33 +0000

    fix

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free
+ * @head: node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *tmp;
+
+	if (!head)
+		return (NULL);
+
+	node = *head;
+
+	while (node)
+	{
+		tmp = node;
+		node = node->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
