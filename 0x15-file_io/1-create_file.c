72504f8b53dfa1d918b20aac251dfc3d286d7160
Feli <srgatoiscool@gmail.com>
2019-11-12 22:42:50 +0000

    added task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	if (!s)
+		return (0);
+
+    for (i = 0; s[i]; i++)
+            ;
+	return (i);
+}
+/**
+ *
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fd;
+    ssize_t bytes = 0, textLen = _strlen(text_content);
+
+    if (!filename)
+        return (0);
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+        return (0);
+    if (textLen)
+        write(fd, text_content, textLen);
+    close(fd);
+    if (bytes == textLen)
+        return (1);
+    return (-1);
+}
