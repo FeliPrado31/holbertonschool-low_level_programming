cf92a0448004c3706a1af9bd6d0fcc437da01b1b
FeliPrado31 <srgatosicool@gmail.com>
2019-11-13 02:19:17 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,23 +1,26 @@
 #include "holberton.h"
 /**
- * create_file - function that creates a file
- * @filename: file name
- * @text_content: text content
+ * create_file - that creates a file.
+ * @filename: char
+ * @text_content: char
  * Return: return
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytes = 0, textLen;
+	int fd, text_Len, bytes;
 
-	if (!filename || !text_content)
+	if (filename == NULL)
 		return (-1);
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (text_content == NULL)
+		return (1);
 	if (fd == -1)
 		return (-1);
-	for (textLen = 0; text_content[textLen] != '\0'; textLen++)
-		;
-	bytes = write(fd, text_content, textLen);
+	for (text_Len = 0; text_content[text_Len] != '\0'; text_Len++)
+	{
+	}
+	bytes = write(fd, text_content, text_Len);
 	if (bytes == -1)
 	{
 		close(fd);
