7b4d283db65d04d7eec2b02beae964c250774712
Feli <srgatoiscool@gmail.com>
2019-11-13 00:08:02 +0000

    fix task 1 and 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,8 +34,11 @@ int create_file(const char *filename, char *text_content)
         return (0);
     if (textLen)
         bytes = write(fd, text_content, textLen);
-    close(fd);
-    if (bytes == textLen)
+    if (bytes == -1)
+    {
+        close(fd);
         return (-1);
+    }
+    close(fd);
     return (1);
 }
