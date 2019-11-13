495d0dad2e25f26eb5802c2056f11299cfa4308c
FeliPrado31 <srgatosicool@gmail.com>
2019-11-13 02:21:22 +0000

    fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,44 +1,31 @@
 #include "holberton.h"
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	if (!s)
-		return (0);
-
-    for (i = 0; s[i]; i++)
-            ;
-	return (i);
-}
-/**
- * append_text_to_file - function that creates a file
- * @filename: file name
- * @text_content: text content
- * Return: return
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: char
+ * @text_content: char
+ * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t bytes = 0, textLen = _strlen(text_content);
+	int fd, text_Len, bytes;
 
-    if (!filename)
-        return (0);
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (0);
-    if (textLen)
-        bytes = write(fd, text_content, textLen);
-    if (bytes == -1)
-    {
-        close(fd);
-        return (-1);
-    }
-    close(fd);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (text_content == NULL)
+		return (1);
+	if (fd == -1)
+		return (-1);
+	for (text_Len = 0; text_content[text_Len] != '\0'; text_Len++)
+	{
+	}
+	bytes = write(fd, text_content, text_Len);
+	if (bytes == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
