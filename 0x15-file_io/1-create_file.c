fca09f559057995eadce3007c4ebeaf6f4a59bc6
FeliPrado31 <srgatosicool@gmail.com>
2019-11-13 02:15:31 +0000

    fix betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,22 +1,5 @@
 #include "holberton.h"
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	if (!s)
-		return (0);
-
-    for (i = 0; s[i]; i++)
-            ;
-	return (i);
-}
-/**
  * create_file - function that creates a file
  * @filename: file name
  * @text_content: text content
@@ -24,21 +7,22 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t bytes = 0, textLen = _strlen(text_content);
+	int fd;
+	ssize_t bytes = 0, textLen;
 
-    if (!filename)
-        return (0);
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (0);
-    if (textLen)
-        bytes = write(fd, text_content, textLen);
-    if (bytes == -1)
-    {
-        close(fd);
-        return (-1);
-    }
-    close(fd);
-    return (1);
+	if (!filename || !text_content)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	for (textLen = 0; text_content[textLen] != '\0'; textLen++)
+		;
+	bytes = write(fd, text_content, textLen);
+	if (bytes == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
