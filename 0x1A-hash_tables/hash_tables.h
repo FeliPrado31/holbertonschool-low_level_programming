1d6dfed060da1f7a3caa75de9aafa64d326b4eb6
FeliPrado31 <srgatosicool@gmail.com>
2020-01-18 17:23:56 +0000

    add #2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,8 @@
 #ifndef _HASH_
 #define _HASH_
+
 /* Including library */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -37,4 +39,11 @@ typedef struct hash_table_s
 
 /*mandatory tatks*/
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
 #endif
