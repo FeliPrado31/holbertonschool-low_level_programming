463fe0c76cce383b499b28855d3ca026407b7a6a
Feli <srgatoiscool@gmail.com>
2020-04-16 19:50:13 -0400

    test

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - 
+ * 
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, l, r;
+
+	if (!array)
+		return (-1);
+
+	for (l = 0; r = size - 1; r >= l;)
+	{
+		printf("Search in array: ");
+		for (i = l; i < r; i++)
+			printf("%d, " array[i]);
+		printf("%d\n", array[i]);
+
+		i = l + (r - l) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
+	}
+	return (-1);
+}
