0a3349b59d57bd7023f42694df4a5391ab1d8c77
Feli <srgatoiscool@gmail.com>
2020-04-16 19:34:23 -0400

    add first

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+  * linear_search - do a linear search
+  * @array: array
+  * @size: size
+  * @value: value
+  * Return: vaue
+  */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
